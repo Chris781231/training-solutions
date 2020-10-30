f3bc89d432cb32a8ea69a6ceeee2a24d5773a586
LigetiKaroly <ligeti.karoly78@gmail.com>
2020-10-30 16:03:58 +0100

    Add Client class app

---
+++ src/main/java/classstructureattributes/ClientMain.java
@@ -0,0 +1,28 @@
+package classstructureattributes;
+
+import java.util.Scanner;
+
+public class ClientMain {
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Client Class App\n");
+
+        System.out.println("What's your name?");
+        client.name = scanner.nextLine();
+
+        System.out.println("What's your year of birth?");
+        client.year = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("What's your address?");
+        client.address = scanner.nextLine();
+
+        System.out.println("Check:");
+        System.out.println("Your name is " + client.name);
+        System.out.println("Your year of birth is " + client.year);
+        System.out.println("Your address is " + client.address);
+    }
+}
