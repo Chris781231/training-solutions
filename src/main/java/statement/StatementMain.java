ea266c44a32c3710c435576360e47d6df979d395
LigetiKaroly <ligeti.karoly78@gmail.com>
2020-11-01 09:43:21 +0100

    Add StatementMain code, examples for statements.

---
+++ src/main/java/statement/StatementMain.java
@@ -0,0 +1,16 @@
+package statement;
+
+public class StatementMain {
+
+    public static void main(String[] args) {
+        int x = 5 + 6;
+        int y = 11 - x;
+        int z = 8;
+
+        boolean b = x > y;
+        boolean c = b || (z > 5);
+
+        z++;
+    }
+
+}
