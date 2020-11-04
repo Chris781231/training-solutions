3e831f5825afefbd08a3d4581c97096a944e495f
LigetiKaroly <ligeti.karoly78@gmail.com>
2020-11-04 17:05:14 +0100

    Fix Registration app

--- src/main/java/stringtype/registration/Registration.java
+++ src/main/java/stringtype/registration/Registration.java
@@ -9,14 +9,14 @@ public class Registration {
         // Request username, password, email
         Scanner scanner = new Scanner(System.in);
 
-//        System.out.print("Username: ");
-//        String username = scanner.nextLine();
-//
-//        System.out.print("Password (min 8 characters): ");
-//        String password1 = scanner.nextLine();
-//
-//        System.out.print("Password again: ");
-//        String password2 = scanner.nextLine();
+        System.out.print("Username: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Password (min 8 characters): ");
+        String password1 = scanner.nextLine();
+
+        System.out.print("Password again: ");
+        String password2 = scanner.nextLine();
 
         System.out.print("Email: ");
         String email = scanner.nextLine();
@@ -24,12 +24,12 @@ public class Registration {
         // Verification
         UserValidator uv = new UserValidator();
 
-//        boolean validUsername = uv.isValidUsername(username);
-//        boolean validPassword = uv.isValidPassword(password1, password2);
+        boolean validUsername = uv.isValidUsername(username);
+        boolean validPassword = uv.isValidPassword(password1, password2);
         boolean validEmail = uv.isValidEmail(email);
 
-//        System.out.println("Username is valid: " + validUsername);
-//        System.out.println("Password is valid: " + validPassword);
+        System.out.println("Username is valid: " + validUsername);
+        System.out.println("Password is valid: " + validPassword);
         System.out.println(validEmail);
 
 
