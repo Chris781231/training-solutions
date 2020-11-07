3dac187b52870ba468fc05ceeebc43e837fa9aef
LigetiKaroly <ligeti.karoly78@gmail.com>
2020-11-07 10:20:06 +0100

    Add ArrayHandler solutions

---
+++ src/main/java/array/ArrayHandler.java
@@ -0,0 +1,22 @@
+package array;
+
+public class ArrayHandler {
+
+    public boolean contains(int[] source, int itemToFind) {
+        for (int item: source) {
+            if (item == itemToFind) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int find(int[] source, int itemToFind) {
+        for (int i = 0; i < source.length; i++) {
+            if (source[i] == itemToFind) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
