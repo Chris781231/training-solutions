9288dbb55b77267c9a078dfae9564d9d1b7c170d
LigetiKaroly <ligeti.karoly78@gmail.com>
2020-11-09 16:01:03 +0100

    Add Division solution

--- src/main/java/literals/LiteralsMain.java
+++ src/main/java/literals/LiteralsMain.java
@@ -4,9 +4,21 @@ public class LiteralsMain {
 
     public static void main(String[] args) {
 
+        // Concatenation
         System.out.println("1" + "2");
         System.out.println("1" + 2);
         System.out.println(1 + "2");
         System.out.println("1".concat("2"));
+
+        // Division
+        double quotient = 3 / 4;        // The result is 0.0, because both of 3 and 4 are integer type literal, so the result will be an integer, even though I store it in a double type.
+        System.out.println(quotient);
+        quotient = (double) 3 / 4;      // The Solution is that one of the members of the literal must be of type double.
+        System.out.println(quotient);
+        // OR
+        quotient = 3.0 / 4;               // or (3 / 4.0)
+        System.out.println(quotient);
+
+
     }
 }
