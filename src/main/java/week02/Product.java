f4b271dc132a380ae6ee6d5c9f6781129378a80a
LigetiKaroly <ligeti.karoly78@gmail.com>
2020-11-12 21:20:58 +0100

    Modify week02 - Product solution... - Creates 'static void printComparison.java'.

--- src/main/java/week02/Product.java
+++ src/main/java/week02/Product.java
@@ -30,20 +30,19 @@ public class Product {
         return "[Name: " + getName() + ", Code: " + getCode() + "]";
     }
 
+    public static void printComparison(Product prod, Product anotherProd) {
+        System.out.print(prod + " & " + anotherProd);
+        System.out.println(prod.areTheyEqual(anotherProd) ? " are equal" : " are not equal");
+    }
+
     public static void main(String[] args) {
 
         Product prod1 = new Product("Milk", "0091 3456728 2");
         Product prod2 = new Product("Bread", "0091 34567 2");
         Product prod3 = new Product("Bread", "0091 345674 2");
 
-        String equalString = " are equal";
-        String notEqualString = " are not equal";
-
-        System.out.print(prod1 + " & " + prod2);
-        System.out.println(prod1.areTheyEqual(prod2) ? equalString : notEqualString);
-        System.out.print(prod1 + " & " + prod3);
-        System.out.println(prod1.areTheyEqual(prod3) ? equalString : notEqualString);
-        System.out.print(prod2 + " & " + prod3);
-        System.out.println(prod2.areTheyEqual(prod3) ? equalString : notEqualString);
+        printComparison(prod1, prod2);
+        printComparison(prod1, prod3);
+        printComparison(prod2, prod3);
     }
 }
