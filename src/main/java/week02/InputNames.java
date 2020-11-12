f4644862c70bdfad371a28f7e04e81566edd25d7
LigetiKaroly <ligeti.karoly78@gmail.com>
2020-11-12 22:12:29 +0100

    Add week02 - InputNames solution.

---
+++ src/main/java/week02/InputNames.java
@@ -0,0 +1,24 @@
+package week02;
+
+import java.util.Scanner;
+
+public class InputNames {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int arraySize = 5;
+        String[] names = new String[arraySize];
+
+        System.out.println("Give me " + arraySize + " names!");
+
+        for (int i = 0; i < arraySize; i++) {
+            System.out.print((i + 1) + ": ");
+            names[i] = scanner.nextLine();
+        }
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
