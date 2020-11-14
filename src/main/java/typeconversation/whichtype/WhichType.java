f5bafa75a8e57a5021beb87fcd668acf0cdde1f9
LigetiKaroly <ligeti.karoly78@gmail.com>
2020-11-14 15:04:51 +0100

    Add WhichType solution

---
+++ src/main/java/typeconversation/whichtype/WhichType.java
@@ -0,0 +1,19 @@
+package typeconversation.whichtype;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WhichType {
+
+    public List<Type> whichType(String s) {
+        List<Type> suitableTypes = new ArrayList<>();
+
+        for (Type type : Type.values()) {
+            if (type.getMinValue() <= Long.parseLong(s) && type.getMaxValue() >= Long.parseLong(s)) {
+                suitableTypes.add(type);
+            }
+        }
+
+        return suitableTypes;
+    }
+}
