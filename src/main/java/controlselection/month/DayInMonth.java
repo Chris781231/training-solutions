be8257ada118506e22d4483254646b14885594a1
LigetiKaroly <ligeti.karoly78@gmail.com>
2020-11-15 11:37:32 +0100

    Add Greetings solution

---
+++ src/main/java/controlselection/month/DayInMonth.java
@@ -0,0 +1,30 @@
+package controlselection.month;
+
+public class DayInMonth {
+
+
+    public int numberOfDays(int year, String month) {
+        switch (month.toLowerCase()) {
+            case "január":
+            case "március":
+            case "május":
+            case "július":
+            case "augusztus":
+            case "október":
+            case "december":
+                return 31;
+            case "április":
+            case "június":
+            case "szeptember":
+            case "november":
+                return 30;
+            case "február":
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                    return 29;
+                }
+                return 28;
+            default:
+                throw new IllegalArgumentException("Invalid month: " + month);
+        }
+    }
+}
