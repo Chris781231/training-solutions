8aeb77cf23abc54b2af579013fff736c56542c2a
LigetiKaroly <ligeti.karoly78@gmail.com>
2020-11-15 14:42:15 +0100

    Add 'Dictionary' solution

---
+++ src/main/java/controliteration/dictionary/Dictionary.java
@@ -0,0 +1,32 @@
+package controliteration.dictionary;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dictionary {
+
+    private List<DictionaryItem> dictionaryItems = new ArrayList<>();
+
+    public void addItem(String word, List<String> translations) {
+        if (dictionaryItems.size() == 0) {
+            dictionaryItems.add(new DictionaryItem(word, translations));
+        } else {
+            for (DictionaryItem item : dictionaryItems) {
+                if (!item.getWord().equals(word)) {
+                    dictionaryItems.add(new DictionaryItem(word, translations));
+                } else {
+                    item.addTranslations(translations);
+                }
+            }
+        }
+    }
+
+    public List<String> findTranslations(String word) {
+        for (DictionaryItem translations : dictionaryItems) {
+            if (translations.getWord().equals(word)) {
+                return translations.getTranslations();
+            }
+        }
+        return null;
+    }
+}
