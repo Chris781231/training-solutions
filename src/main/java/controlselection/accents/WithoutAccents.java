50620b2af0cb8f2f97cb8344bf9ef9df90ec3654
LigetiKaroly <ligeti.karoly78@gmail.com>
2020-11-15 12:22:16 +0100

    Add 'Accents' solution

---
+++ src/main/java/controlselection/accents/WithoutAccents.java
@@ -0,0 +1,43 @@
+package controlselection.accents;
+
+public class WithoutAccents {
+
+
+    public char convertToCharWithoutAccents(char character) {
+        switch (character) {
+            // Small caps
+            case 'á':
+                return 'a';
+            case 'é':
+                return 'e';
+            case 'í':
+                return 'i';
+            case 'ó':
+            case 'ö':
+            case 'ő':
+                return 'o';
+            case 'ú':
+            case 'ü':
+            case 'ű':
+                return 'u';
+
+            // Large caps
+            case 'Á':
+                return 'A';
+            case 'É':
+                return 'E';
+            case 'Í':
+                return 'I';
+            case 'Ó':
+            case 'Ö':
+            case 'Ő':
+                return 'O';
+            case 'Ú':
+            case 'Ü':
+            case 'Ű':
+                return 'U';
+            default:
+                return character;
+        }
+    }
+}
