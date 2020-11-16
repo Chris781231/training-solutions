2a305b5bd7af1e67bdf5076ea5b72ae1b3246f26
LigetiKaroly <ligeti.karoly78@gmail.com>
2020-11-16 10:19:07 +0100

    Add `Strings` solution

---
+++ src/main/java/stringbasic/StringCreator.java
@@ -0,0 +1,13 @@
+package stringbasic;
+
+public class StringCreator {
+
+
+    public String createStringForPool() {
+        return "Charlie Parker";
+    }
+
+    public String createStringForHeap() {
+        return new String("Charlie Parker");
+    }
+}
---
+++ src/test/java/stringbasic/StringCreatorTest.java
@@ -0,0 +1,30 @@
+package stringbasic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StringCreatorTest {
+
+    @Test
+    public void sameStringsInPoolShouldBeTheSameObject() {
+        //Given
+        StringCreator sc = new StringCreator();
+
+        //Then
+        assertTrue(sc.createStringForPool() == sc.createStringForPool());
+        assertFalse(sc.createStringForHeap() == sc.createStringForHeap());
+        assertTrue(sc.createStringForPool().equals(sc.createStringForHeap()));
+    }
+
+    @Test
+    public void sameStringsShouldBeTheSameByCanonicalRepresentation() {
+        //Given
+        StringCreator sc = new StringCreator();
+
+        //Then
+        assertTrue(sc.createStringForHeap().intern() == sc.createStringForHeap().intern());
+        assertTrue(sc.createStringForHeap().intern() == sc.createStringForPool().intern());
+    }
+}
