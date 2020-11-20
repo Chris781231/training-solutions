291bd7b34a2550615d779e985cc5796eaf367904
LigetiKaroly <ligeti.karoly78@gmail.com>
2020-11-20 15:13:16 +0100

    Modify Library.java.

--- src/main/java/filescanner/Library.java
+++ src/main/java/filescanner/Library.java
@@ -18,24 +18,7 @@ public class Library {
 
     }
 
-    public void loadFromFile2() {
-        try(Scanner scanner = new Scanner(Library.class.getResourceAsStream("/books.csv"))) {
-            scanner.useDelimiter(";|(\r\n)|\n");
-            while (scanner.hasNextLine()) {
-                String regNum = scanner.next();
-                String author = scanner.next();
-                String title = scanner.next();
-                int yearOfPublish = scanner.nextInt();
-                books.add(new Book(regNum, author, title, yearOfPublish));
-            }
-        }
-    }
-
     public List<Book> getBooks() {
         return books;
-    }
-
-    public static void main(String[] args) {
-        new Library().loadFromFile();
     }
 }
--- src/test/java/filescanner/LibraryTest.java
+++ src/test/java/filescanner/LibraryTest.java
@@ -9,7 +9,7 @@ class LibraryTest {
     public void testLoadFromFile() {
         Library library = new Library();
 
-        library.loadFromFile2();
+        library.loadFromFile();
 
         assertEquals(4, library.getBooks().size());
         assertEquals("Robert C. Martin", library.getBooks().get(2).getAuthor());
