9b3a239638ba69c0819d2c9c66b0bae3a81512ba
LigetiKaroly <ligeti.karoly78@gmail.com>
2020-11-22 12:11:05 +0100

    Add 'week03.Opertion' solution

---
+++ src/main/java/week03/Operation.java
@@ -0,0 +1,22 @@
+package week03;
+
+public class Operation {
+
+    private final int leftOperand;
+    private final int rightOperand;
+
+    public Operation(String literal) {
+        if (literal.isBlank()) {
+            throw new IllegalArgumentException("Nem lehet üres paraméter");
+        }
+        if (!literal.contains("+")) {
+            throw new IllegalArgumentException("Csak összeadás ('+') megengedett!");
+        }
+        leftOperand = Integer.parseInt(literal.substring(0, literal.indexOf("+")).trim());
+        rightOperand = Integer.parseInt(literal.substring(literal.indexOf("+") + 1).trim());
+    }
+
+    public int getResult() {
+        return leftOperand + rightOperand;
+    }
+}
---
+++ src/test/java/week03/OperationTest.java
@@ -0,0 +1,27 @@
+package week03;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class OperationTest {
+
+    @Test
+    void testGetResult() {
+        assertEquals(8, new Operation("3+5").getResult());
+        assertEquals(30, new Operation(" 14 + 16 ").getResult());
+    }
+
+    @Test
+    void emptyOrContainsOnlyWhiteSpaceLiteralShouldThrowException() {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> new Operation(" \n\t"));
+        ex = assertThrows(IllegalArgumentException.class, () -> new Operation(""));
+    }
+
+    @Test
+    void literalThatNotContainsPlusSignShouldThrowException() {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> new Operation("3-5"));
+        ex = assertThrows(IllegalArgumentException.class, () -> new Operation("35"));
+    }
+}
