a0c8d487fb65dfaa19751e97cacd58e3368297c3
LigetiKaroly <ligeti.karoly78@gmail.com>
2020-11-26 18:06:04 +0100

    Modify 'defaultconstructor.simpledate.SimpleDate' solution  - rename SimpleDate to SimpleDateTemp  - create new SimpleDate class based on teacher suggestions

--- src/main/java/defaultconstructor/simpledate/SimpleDate.java
+++ src/main/java/defaultconstructor/simpledate/SimpleDate.java
@@ -6,18 +6,14 @@ public class SimpleDate {
     private int month;
     private int day;
 
-
-
     public void setDate(int year, int month, int day) {
-        boolean successYear = setYear(year);
-        boolean successMonth = setMonth(month);
-        boolean successDay = setDay(day);
-        if (!successYear || !successMonth || !successDay) {
+        if (!isCorrect(year, month, day)) {
             throw new IllegalArgumentException("One or more given parameter cannot be applied! " + year + ", " + month + ", " + day);
         }
+        this.year = year;
+        this.month = month;
+        this.day = day;
     }
-
-
 
     public int getYear() {
         return year;
@@ -33,67 +29,38 @@ public class SimpleDate {
 
 
 
-    private boolean setYear(int year) {
+    private boolean isCorrect(int year, int month, int day) {
+        boolean isCorrect = true;
+
         if (year < 1900) {
-            return false;
+            isCorrect = false;
         }
-        this.year = year;
-        return true;
-    }
 
-    private boolean setMonth(int month) {
         if (month < 1 || month > 12) {
-            return false;
+            isCorrect = false;
         }
-        this.month = month;
-        return true;
+
+        if (day < 1 || day > calculateMonthLength(year, month)) {
+            isCorrect = false;
+        }
+
+        return isCorrect;
     }
 
-    private boolean setDay(int day) {
-        boolean success;
+    private int calculateMonthLength(int year, int month) {
         switch (month) {
             case 1, 3, 5, 7, 8, 10, 12 -> {
-                success = setDayOfThirtyOneDayMonth(day);
+                return 31;
             }
             case 4, 6, 9, 11 -> {
-                success = setDayOfThirtyDayMonth(day);
-            }
-            case 2 -> {
-                success = setDayOfFebruary(day);
+                return 30;
             }
             default -> {
-                success = false;
+                if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                    return 29;
+                }
+                return 28;
             }
         }
-        return success;
-    }
-
-
-
-    private boolean setDayOfThirtyOneDayMonth(int day) {
-        if (day < 1 || day > 31) {
-            return false;
-        }
-        this.day = day;
-        return true;
-    }
-
-    private boolean setDayOfThirtyDayMonth(int day) {
-        if (day < 1 || day > 30) {
-            return false;
-        }
-        this.day = day;
-        return true;
-    }
-
-    private boolean setDayOfFebruary(int day) {
-        boolean isLeapYear = ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
-
-        if ((isLeapYear && (day < 1 || day > 29)) || (!isLeapYear && (day < 1 || day > 28))) {
-            return false;
-        }
-
-        this.day = day;
-        return true;
     }
 }
---
+++ src/main/java/defaultconstructor/simpledate/SimpleDateTemp.java
@@ -0,0 +1,99 @@
+package defaultconstructor.simpledate;
+
+public class SimpleDateTemp {
+
+    private int year;
+    private int month;
+    private int day;
+
+
+
+    public void setDate(int year, int month, int day) {
+        boolean successYear = setYear(year);
+        boolean successMonth = setMonth(month);
+        boolean successDay = setDay(day);
+        if (!successYear || !successMonth || !successDay) {
+            throw new IllegalArgumentException("One or more given parameter cannot be applied! " + year + ", " + month + ", " + day);
+        }
+    }
+
+
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+
+
+    private boolean setYear(int year) {
+        if (year < 1900) {
+            return false;
+        }
+        this.year = year;
+        return true;
+    }
+
+    private boolean setMonth(int month) {
+        if (month < 1 || month > 12) {
+            return false;
+        }
+        this.month = month;
+        return true;
+    }
+
+    private boolean setDay(int day) {
+        boolean success;
+        switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12 -> {
+                success = setDayOfThirtyOneDayMonth(day);
+            }
+            case 4, 6, 9, 11 -> {
+                success = setDayOfThirtyDayMonth(day);
+            }
+            case 2 -> {
+                success = setDayOfFebruary(day);
+            }
+            default -> {
+                success = false;
+            }
+        }
+        return success;
+    }
+
+
+
+    private boolean setDayOfThirtyOneDayMonth(int day) {
+        if (day < 1 || day > 31) {
+            return false;
+        }
+        this.day = day;
+        return true;
+    }
+
+    private boolean setDayOfThirtyDayMonth(int day) {
+        if (day < 1 || day > 30) {
+            return false;
+        }
+        this.day = day;
+        return true;
+    }
+
+    private boolean setDayOfFebruary(int day) {
+        boolean isLeapYear = ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
+
+        if ((isLeapYear && (day < 1 || day > 29)) || (!isLeapYear && (day < 1 || day > 28))) {
+            return false;
+        }
+
+        this.day = day;
+        return true;
+    }
+}
