6bc87e1f00cf7436f1a716c88233b4dff24705ce
LigetiKaroly <ligeti.karoly78@gmail.com>
2020-12-02 10:34:40 +0100

    Modify 'week06d03.midlevel' solution

--- src/main/java/week06d03/midlevel/WordEraser.java
+++ src/main/java/week06d03/midlevel/WordEraser.java
@@ -3,6 +3,13 @@ package week06d03.midlevel;
 public class WordEraser {
 
     public String eraseWord(String words, String word) {
+        if (words == null || word == null) {
+            throw new IllegalArgumentException("Parameters of eraseWord must not be null!");
+        }
+        if (words.isBlank() || word.isBlank()) {
+            return words;
+        }
+
         StringBuilder changedWords = new StringBuilder();
 
         for (String wordsItem : words.split(" ")) {
--- src/test/java/week06d03/midlevel/WordEraserTest.java
+++ src/test/java/week06d03/midlevel/WordEraserTest.java
@@ -2,17 +2,30 @@ package week06d03.midlevel;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class WordEraserTest {
 
+    WordEraser wordEraser = new WordEraser();
+
     @Test
     void wordEraserTest() {
-        WordEraser wordEraser = new WordEraser();
         String words = "Lorem ipsum dolor sit amet, libris ipsum putent perpetua ipsum ut mei, ipsum nam ut ipsum quis principes ipsum persecuti. Qui ne dicant graece inermis.";
         String word = "ipsum";
 
         assertEquals("Lorem dolor sit amet, libris putent perpetua ut mei, nam ut quis principes persecuti. Qui ne dicant graece inermis.",
                 wordEraser.eraseWord(words, word));
+    }
+
+    @Test
+    void nullCheckTest() {
+        assertThrows(IllegalArgumentException.class, () -> wordEraser.eraseWord(null, "ipsum"));
+        assertThrows(IllegalArgumentException.class, () -> wordEraser.eraseWord("Lorem ipsum dolor sit amet", null));
+    }
+
+    @Test
+    void emptyCheckTest() {
+        assertEquals("", wordEraser.eraseWord("", "ipsum"));
+        assertEquals("Lorem ipsum dolor sit amet", wordEraser.eraseWord("Lorem ipsum dolor sit amet", ""));
     }
 }
