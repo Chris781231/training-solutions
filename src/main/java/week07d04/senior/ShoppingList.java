70d5e7f68e419ba3530fb1f0804c1c39115a0ad8
LigetiKaroly <ligeti.karoly78@gmail.com>
2020-12-12 00:10:44 +0100

    Add checkSlashes() to 'week07d04.senior' solution

--- src/main/java/week07d04/senior/ShoppingList.java
+++ src/main/java/week07d04/senior/ShoppingList.java
@@ -9,6 +9,8 @@ public class ShoppingList {
      * @return Sum of amount multiply by unit price from scanned file
      */
     public long calculateSum(String path) {
+        path = checkSlashes(path);
+
         long sum = 0;
 
         try (Scanner fileScanner = new Scanner(ShoppingList.class.getResourceAsStream(path + "shopping_list.csv"))) {
@@ -20,6 +22,16 @@ public class ShoppingList {
         }
 
         return sum;
+    }
+
+    private String checkSlashes(String path) {
+        if (path.charAt(0) != '/') {
+            path = '/' + path;
+        }
+        if (path.charAt(path.length()-1) != '/') {
+            path += '/';
+        }
+        return path;
     }
 
     private long getSum(long sum, Scanner fileScanner) {
