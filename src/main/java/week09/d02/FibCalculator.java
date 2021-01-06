bc18d335578bd214d9737d1fd9f104a36bdfeaee
LigetiKaroly <ligeti.karoly78@gmail.com>
2021-01-06 15:17:20 +0100

    Fix 'week09.d02' solution

--- src/main/java/week09/d02/FibCalculator.java
+++ src/main/java/week09/d02/FibCalculator.java
@@ -2,24 +2,38 @@ package week09.d02;
 
 public class FibCalculator {
 
-    public long sumEvens(int bound) {
-        if (bound < 0) throw new IllegalArgumentException("Number must be natural number");
-        if (bound == 0) return 0;
-        if (bound == 1) return 1;
+    public static int[] FIB_NUMS = new int[1000];
 
-        long nMinusTwoFib = 0;
-        long nMinusOneFib = 1;
-        long nthFib = nMinusTwoFib + nMinusOneFib;
-        long sumFib = 0;
-
-        while (nthFib <= bound) {
-            if (nthFib % 2 == 0) {
-                sumFib += nthFib;
-            }
-            nMinusTwoFib = nMinusOneFib;
-            nMinusOneFib = nthFib;
-            nthFib = nMinusTwoFib + nMinusOneFib;
+    public int sumEvens(int bound) {
+        if (bound < 0 || bound >= FIB_NUMS.length) {
+            throw new IllegalArgumentException("bound must be between 0 and 999");
         }
-        return sumFib;
+        int sum = 0;
+        int fibN = 0;
+        while (fibCalc(fibN) < bound) {
+            if (fibN % 2 == 0) {
+                sum += FIB_NUMS[fibN];
+            }
+            fibN++;
+        }
+        return sum;
+    }
+
+    private int fibCalc(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (FIB_NUMS[n] == 0) {
+            FIB_NUMS[n] = fibCalc(n - 1) + fibCalc(n - 2);
+        }
+        return FIB_NUMS[n];
+    }
+
+    public static void main(String[] args) {
+        FibCalculator fibCalculator = new FibCalculator();
+        System.out.println(fibCalculator.sumEvens(2));
     }
 }
