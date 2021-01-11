80470a277e59fa664dafc1a760aec73a35d75309
LigetiKaroly <ligeti.karoly78@gmail.com>
2021-01-11 16:17:31 +0100

    Add 'week10.d03.midlevel' solution

---
+++ src/main/java/week10/d03/midlevel/Calculator.java
@@ -0,0 +1,29 @@
+package week10.d03.midlevel;
+
+public class Calculator {
+
+    public int findMinSum(int[] numbers) {
+        if (numbers.length < 4) {
+            throw new IllegalArgumentException("Too few elements are in array");
+        }
+
+        sort(numbers);
+        int sum = 0;
+        for (int i = 0; i < 4; i++) {
+            sum += numbers[i];
+        }
+        return sum;
+    }
+
+    private void sort(int[] numbers) {
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - i - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ src/test/java/week10/d03/midlevel/CalculatorTest.java
@@ -0,0 +1,14 @@
+package week10.d03.midlevel;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void findMinSumTest() {
+        int[] numbers = new int[]{1, 3, 5, 7, 9};
+        assertEquals(16, new Calculator().findMinSum(numbers));
+    }
+}
