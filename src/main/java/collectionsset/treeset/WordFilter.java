7bc135713e3fa6105eddbc6a8c0d783d38436ed9
LigetiKaroly <ligeti.karoly78@gmail.com>
2021-01-19 14:42:36 +0100

    Add 'collectionsset.treeset' solution

---
+++ src/main/java/collectionsset/treeset/WordFilter.java
@@ -0,0 +1,13 @@
+package collectionsset.treeset;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class WordFilter {
+
+
+    public Set<String> filterWords(String[] randomStrings) {
+        return new TreeSet<>(Arrays.asList(randomStrings));
+    }
+}
