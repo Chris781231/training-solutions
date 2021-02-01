ca90867d631d5f953fefbc4825369e071fc1b68c
LigetiKaroly <ligeti.karoly78@gmail.com>
2021-02-01 15:33:57 +0100

    Add 'week14.d01.midlevel' solution

---
+++ src/main/java/week14/d01/Indexer.java
@@ -0,0 +1,25 @@
+package week14.d01;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Indexer {
+
+    public Map<Character, List<String>> index(List<String> names) {
+        Map<Character, List<String>> result = new TreeMap<>();
+
+        for (String name : names) {
+            char key = name.charAt(0);
+            if (!result.containsKey(key)) {
+                result.put(key, new ArrayList<>());
+            }
+            List<String> nameList = result.get(key);
+            nameList.add(name);
+            result.put(key, nameList);
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/week14/d01/IndexerTest.java
@@ -0,0 +1,16 @@
+package week14.d01;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class IndexerTest {
+
+    @Test
+    void indexTest() {
+        String[] names = {"Odon", "Lujza", "Abraham", "Magdolna", "Márk"};
+        assertEquals("{A=[Abraham], L=[Lujza], M=[Magdolna, Márk], O=[Odon]}", new Indexer().index(Arrays.asList(names)).toString());
+    }
+}
